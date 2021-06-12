5c064b06532bbeb4df19259521aea61ae11dc6fc
asepac01 <lorelyangie01@gmail.com>
2021-06-12 18:38:17 -0500

    Creo 3 objetos para interactuar con la clase Fraccion.

--- clase_03_06_2021.py
+++ clase_03_06_2021.py
@@ -6,3 +6,8 @@ class Fraccion:
 
     def show(self):
         return '» {} ÷ {}'.format(self.nume, self.deno)
+
+
+obj_fraccion1 = Fraccion(21, 23)
+obj_fraccion2 = Fraccion(35, 36)
+obj_fraccion3 = Fraccion(5, 5)
