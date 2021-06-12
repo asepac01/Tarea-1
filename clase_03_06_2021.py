32bea1a47bfa40c3834cfa74090ce7c93995bb45
asepac01 <lorelyangie01@gmail.com>
2021-06-12 18:37:30 -0500

    Creo el metodo show de la clase Fraccion.

--- clase_03_06_2021.py
+++ clase_03_06_2021.py
@@ -3,3 +3,6 @@ class Fraccion:
     def __init__(self, numerador, denominador):
         self.nume = numerador
         self.deno = denominador
+
+    def show(self):
+        return '» {} ÷ {}'.format(self.nume, self.deno)
